573ba5124e0f75703bbfd59bf4c8a35e19093d46
Wolemercy <wolemercy@gmail.com>
2022-10-27 11:46:28 +0100

    Add Name Concatenation

---
+++ twenty-questions/OneToTen.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace twenty_questions
+{
+    public class OneToTen
+    {
+        public OneToTen()
+        {
+        }
+
+        public void NameConcatenation()
+        {
+            Console.WriteLine("Please enter your First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Last Name: ");
+            string lastName = Console.ReadLine();
+
+            int age;
+            while (true)
+            {
+                Console.WriteLine("Enter your Age: ");
+                try
+                {
+                    age = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter a valid number as your age");
+                }
+            }
+
+            int currentYear = DateTime.Now.Year;
+            int birthYear = currentYear - age;
+
+            string gender;
+            string genderNoun;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please choose a gender:\n1. Male (1/M/Male)\n2. Female(2/F/Female)");
+                    gender = Console.ReadLine().ToLower();
+
+                    switch (gender)
+                    {
+                        case "1":
+                        case "m":
+                        case "male":
+                            genderNoun = "Son";
+                            break;
+
+                        case "2":
+                        case "f":
+                        case "female":
+                            genderNoun = "Daughter";
+                            break;
+                        default:
+                            throw new FormatException();
+                    }
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter a valid gender");
+                }
+            }
+
+            Console.WriteLine($"Welcome, {firstName}, {genderNoun} of {lastName}, born in {birthYear}");
+            Console.Read();
+        }
+    }
+}
+
---
+++ twenty-questions/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using twenty_questions;
+Console.WriteLine("Hello, World!");
+
+OneToTen oneToTen = new OneToTen();
+oneToTen.NameConcatenation();
+
